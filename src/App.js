8f47a347ff7aadf192db29c4ee35d4fc0667a539
adeenulhaq3 <adeen553@gmail.com>
2018-10-11 21:57:31 +0500

    Adding Textfields

--- src/App.js
+++ src/App.js
@@ -1,76 +1,146 @@
 import React, { Component } from 'react';
 import './App.css';
+import AppBar from './components/PrimaryAppBar/PrimaryAppBar';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import AddIcon from '@material-ui/icons/Add';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid/Grid';
 import IconButton from '@material-ui/core/IconButton';
-import DeleteIcon from '@material-ui/icons/Delete';
-import Icon from '@material-ui/core/Icon';
-import BottomNavigation from '@material-ui/core/BottomNavigation';
-import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import RestoreIcon from '@material-ui/icons/Restore';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import LocationOnIcon from '@material-ui/icons/LocationOn';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControl from '@material-ui/core/FormControl';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import classNames from 'classnames';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { red, amber } from '@material-ui/core/colors';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: amber[500],
+        },
+        secondary: red,
+        type: 'light'
+    }
+})
 
 const styles = theme => ({
-  root: {
-    width: 900,
-  },
-  button: {
-    margin: theme.spacing.unit,
-  },
-  input: {
-    display: 'none',
-  },
+    textField: {
+        marginLeft: theme.spacing.unit,
+        marginRight: theme.spacing.unit,
+        flexBasis: 200,
+    },
+    margin: {
+        margin: theme.spacing.unit,
+    },
+    root: {
+        flexGrow: 1,
+    },
 });
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      value: 'recents'
-    }  
-  }
+    constructor() {
+        super();
 
-  propTypes = {
-    classes: PropTypes.object.isRequired,
-  };
+        this.state = {
+            firstName: '',
+            lastName: '',
+            email: '',
+            password: '',
+            showPassword: false,
+        }
+    }
 
-  handleChange = (event, value) => {
-    this.setState({ value });
-  };
+    handleChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value });
+    };
 
-  render() {
-    const { classes } = this.props;
-    const { value } = this.state;
-    return (
-      <div>
-        <Button variant="fab" color="primary" aria-label="Add" className={classes.button}>
-          <AddIcon />
-        </Button>
-        <Button variant="contained" color="secondary" className={classes.button}>
-          Delete
-        <DeleteIcon className={classes.rightIcon} />
-        </Button>
-        <TextField label='Email' variant='filled' autoComplete />
-        <Button variant="outlined" color="secondary" className={classes.button}>
-          Secondary
-      </Button>
-        <IconButton className={classes.button} aria-label="Delete">
-          <DeleteIcon />
-        </IconButton>
+    handleClickShowPassword = () => {
+        this.setState(state => ({ showPassword: !state.showPassword }));
+    };
 
-        <BottomNavigation value={value} onChange={this.handleChange} className={classes.root}>
-          <BottomNavigationAction label="Recents" value="recents" icon={<RestoreIcon />} />
-          <BottomNavigationAction label="Favorites" value="favorites" icon={<FavoriteIcon />} />
-          <BottomNavigationAction label="Nearby" value="nearby" icon={<LocationOnIcon />} />
-          <BottomNavigationAction label="Folder" value="folder" icon={<Icon>folder</Icon>} />
-        </BottomNavigation>
-      </div>
-    );
-  }
+    PropTypes = {
+        classes: PropTypes.object.isRequired,
+    };
+
+    render() {
+        const {
+            classes
+        } = this.props;
+
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+            showPassword
+        } = this.state;
+
+        return (
+            <MuiThemeProvider theme={theme}>
+                <AppBar />
+                <div className={classes.root}>
+                    <Grid container>
+                        <Grid item lg={12}>
+                            <TextField
+                                id="standard-name"
+                                label="First Name"
+                                className={classes.textField}
+                                value={ firstName }
+                                onChange={ this.handleChange }
+                                margin="normal"
+                            />
+                        </Grid>
+                        <Grid item lg={12}>
+                            <TextField
+                                id="standard-name"
+                                label="Last Name"
+                                className={classes.textField}
+                                value={ lastName }
+                                onChange={ this.handleChange }
+                            />
+                        </Grid>
+                        <Grid item lg={12}>
+                            <TextField
+                                label="Email"
+                                className={classNames(classes.margin, classes.textField)}
+                                type="email"
+                                name="email"
+                                value={email}
+                                autoComplete="email"
+                                onChange={this.handleChange}
+                            />
+                        </Grid>
+                        <Grid item lg={12}><FormControl className={classNames(classes.margin, classes.textField)}>
+                            <InputLabel htmlFor="adornment-password">Password</InputLabel>
+                            <Input
+                                id="adornment-password"
+                                type={showPassword ? 'text' : 'password'}
+                                value={password}
+                                name='password'
+                                onChange={this.handleChange}
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            aria-label="Toggle password visibility"
+                                            onClick={this.handleClickShowPassword}
+                                        >
+                                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                            />
+                        </FormControl></Grid>
+                    </Grid>
+                </div>
+            </MuiThemeProvider>
+        );
+    }
+
 }
 
 export default withStyles(styles)(App);
